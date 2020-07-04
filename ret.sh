d504efdde4004835423e563872f3ec6bb8e33e34
paragsahu <parag7sahu61@gmail.com>
2020-07-04 08:41:28 +0530

    Assign-day-10-Day10-Sorting

---
+++ ret.sh
@@ -0,0 +1,16 @@
+Live Demo
+#!/bin/sh
+
+# Define your function here
+Hello () {
+   echo "Hello World $1 $2"
+   return 10
+}
+
+# Invoke your function
+Hello Zara Ali
+
+# Capture value returnd by last command
+ret=$?
+
+echo "Return value is $ret"
